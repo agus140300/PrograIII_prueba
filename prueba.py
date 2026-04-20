dcdf41b3b0fa917cda385a3bd63d99ec1517bd02
agus140300 <agustincarrasco31@gmail.com>
2026-04-20 14:48:03 -0300

    agregue funcion restar y modigique mensaje

--- prueba.py
+++ prueba.py
@@ -1,3 +1,7 @@
+def restar(a,b):
+    resta = a -b
+    return resta
+
 def SumarNumero(a, b):
     suma = a + b
     return suma
@@ -6,5 +10,8 @@ if __name__ == "__main__":
     a = int(input("Ingrese el primer número: "))
     b = int(input("Ingrese el segundo número: "))
     resultado = SumarNumero(a, b)
-    print(f"La suma de {a} y {b} es: {resultado}")
+    print(f"El resultado es: ", SumarNumero)
+    print(f"El resultado es: ", restar)
+    
+    
     
